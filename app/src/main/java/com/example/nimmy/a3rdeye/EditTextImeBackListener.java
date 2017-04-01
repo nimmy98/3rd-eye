f64aeaf82064a7c3b863bd13cef136677128dd70
Nimmy <nimmypatel98@gmail.com>
2017-04-01 18:46:31 +0530

    create  a blind application

---
+++ app/src/main/java/com/example/nimmy/a3rdeye/EditTextImeBackListener.java
@@ -0,0 +1,6 @@
+package com.example.nimmy.a3rdeye;
+
+public interface EditTextImeBackListener
+	{
+	public abstract void onImeBack(EditTextBackEvent ctrl, String text);
+	}
---
+++ app/src/main/java/com/example/nimmy/a3rdeye/ITelephony.java
@@ -0,0 +1,9 @@
+package com.example.nimmy.a3rdeye;
+
+/**
+ * Created by Nimmy on 29-03-2017.
+ */
+public class ITelephony {
+    public void endCall() {
+    }
+}
